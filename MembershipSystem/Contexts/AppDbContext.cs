93296f6a27a350dd179a584d8a4cec67f4d5890f
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-07-09 03:34:29 +0300

    Added Identity AppUser, AppRole and AppDbContext

---
+++ MembershipSystem/Contexts/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace MembershipSystem.Contexts
+{
+    public class AppDbContext:IdentityDbContext
+    {
+    }
+}
--- MembershipSystem/Program.cs
+++ MembershipSystem/Program.cs
@@ -1,7 +1,17 @@
+using MembershipSystem.Contexts;
+using MembershipSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+}
+);
+builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
