8726f73df79d87756727203e85591e860c3fa720
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-07-11 03:33:35 +0300

    Add Admin Area

---
+++ MembershipSystem/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MembershipSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserList()
+        {
+            return View();
+        }
+    }
+}
