693de236e936403f3f609fcbb50ba6f7e2d2431d
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-10-05 01:50:08 +0300

    Added User Picture and TagHelper

--- MembershipSystem/ViewModels/UserViewModel.cs
+++ MembershipSystem/ViewModels/UserViewModel.cs
@@ -7,5 +7,6 @@ namespace MembershipSystem.ViewModels
         public string UserName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string? PhoneNumber { get; set; }
+        public string? PictureUrl { get; set; }
     }
 }
